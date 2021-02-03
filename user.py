88debe71ce8ffda2a4c27bc7e7a24b115d2ef194
Wonchan Kim <wonchankim97@gmail.com>
2021-02-03 00:10:55 -0800

    put in all the fields for the users. have to modify the xpaths next

--- user.py
+++ user.py
@@ -39,11 +39,101 @@ for i in range(len(users[:3])):
         fullName = driver.find_element_by_xpath('.//*/h2[@class="full-name"]').text
     except:
         fullName = np.nan
+    try:
+        title = driver.find_element_by_xpath('.//*/div[@class="user-profile-names"]/h3').text
+    except:
+        title = np.nan
+    try:
+        location = driver.find_element_by_xpath('.//*/div[@class="user-profile-location"]').text
+    except:
+        location = np.nan
+    try:
+        website = driver.find_element_by_xpath('.//*/div[@class="user-profile-website"]/a').get_attribute('href')
+    except:
+        website = np.nan
+    try:
+        bio = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        bio = np.nan
+
+    # left off
+    try:
+        creatorType = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        creatorType = np.nan
+    try:
+        patreonURL = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        patreonURL = np.nan
+    try:
+        joined = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        joined = np.nan
+    try:
+        lastPosted = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        lastPosted = np.nan
+    try:
+        lastSeen = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        lastSeen = np.nan
+    try:
+        views = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        views = np.nan
+    try:
+        invitedBy = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        invitedBy = np.nan
+    try:
+        trustLevel = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        trustLevel = np.nan
+    try:
+        daysVisited = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        daysVisited = np.nan
+    try:
+        readTime = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        readTime = np.nan
+    try:
+        recentReadTime = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        recentReadTime = np.nan
+    try:
+        topicsViewed = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        topicsViewed = np.nan
+    try:
+        postsRead = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        postsRead = np.nan
+    try:
+        likesGiven = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        likesGiven = np.nan
+    try:
+        topicsCreated = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        topicsCreated = np.nan
+    try:
+        postsCreated = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        postsCreated = np.nan
+    try:
+        likesReceived = driver.find_element_by_xpath('.//*/div[@class="bio"]/div[@id="ember19"]/p').text
+    except:
+        likesReceived = np.nan
 
     userDict = {}
 
     userDict['username'] = username
     userDict['fullName'] = fullName
+    userDict['title'] = title
+    userDict['location'] = location
+    userDict['website'] = website
+    userDict['bio'] = bio
 
     writer.writerow(userDict)
 
