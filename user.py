c7868a2ca846677f183f7c8d90c62b78f7def4a7
Wonchan Kim <wonchankim97@gmail.com>
2021-02-02 21:18:45 -0800

    all the users get read into a list properly in user.py file

--- user.py
+++ user.py
@@ -7,21 +7,29 @@ import re
 import numpy as np
 import csv
 
-chromeOptions = webdriver.ChromeOptions()
-prefs = {'profile.managed_default_content_settings.images':2}
-chromeOptions.add_experimental_option('prefs', prefs)
-driver = webdriver.Chrome(chrome_options=chromeOptions)
+# read in all the users into a list that we'll loop
+with open('users.csv', 'r') as f:
+    users = f.readlines()
+    users = list(map(lambda x: x.strip('\n'), users))
 
-driver.get('https://www.patreoncommunity.com/u?period=all') # https://www.patreoncommunity.com/
+for i in range(len(users[:3])):
+    # write a csv file for each user
+    csv_file = open('user{0}.csv'.format(i), 'w', encoding='utf-8', newline='')
+    writer = csv.DictWriter(csv_file, 
+                            fieldnames = ['username', 'fullName'. 'title', 'location', 'website',\
+                                'bio', 'creatorType', 'patreonURL', 'joined', 'lastPosted', 'lastSeen',\
+                                'views', 'invitedBy', 'trustLevel', 'daysVisited', 'readTime', 'recentReadTime',\
+                                'topicsViewed', 'postsRead', 'likesGiven', 'topicsCreated', 'postsCreated',\
+                                'likedReceived'])
 
-# write a csv file for each user
-csv_file = open('user{0}.csv'.format(i), 'w', encoding='utf-8', newline='')
-writer = csv.DictWriter(csv_file, 
-                        fieldnames = ['username', 'fullName'. 'title', 'location', 'website',\
-                            'bio', 'creatorType', 'patreonURL', 'joined', 'lastPosted', 'lastSeen',\
-                            'views', 'invitedBy', 'trustLevel', 'daysVisited', 'readTime', 'recentReadTime',\
-                            'topicsViewed', 'postsRead', 'likesGiven', 'topicsCreated', 'postsCreated',\
-                            'likedReceived'])
+    # open up a driver for each user page
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {'profile.managed_default_content_settings.images':2}
+    chromeOptions.add_experimental_option('prefs', prefs)
+    driver = webdriver.Chrome(options=chromeOptions)
+
+    username = 
+
 
 # # scroll down to the very bottom of the page
 # driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
