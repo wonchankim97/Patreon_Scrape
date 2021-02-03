11fc79900e93f2553acb1ae739272b6b870b356f
Wonchan Kim <wonchankim97@gmail.com>
2021-02-02 22:58:14 -0800

    username and fullname tested and written into csv

--- user.py
+++ user.py
@@ -12,23 +12,42 @@ with open('users.csv', 'r') as f:
     users = f.readlines()
     users = list(map(lambda x: x.strip('\n'), users))
 
-for i in range(len(users[:3])):
-    # write a csv file for each user
-    csv_file = open('user{0}.csv'.format(i), 'w', encoding='utf-8', newline='')
-    writer = csv.DictWriter(csv_file, 
-                            fieldnames = ['username', 'fullName'. 'title', 'location', 'website',\
-                                'bio', 'creatorType', 'patreonURL', 'joined', 'lastPosted', 'lastSeen',\
-                                'views', 'invitedBy', 'trustLevel', 'daysVisited', 'readTime', 'recentReadTime',\
-                                'topicsViewed', 'postsRead', 'likesGiven', 'topicsCreated', 'postsCreated',\
-                                'likedReceived'])
+# write a csv file with each user as a row
+csv_file = open('userData.csv', 'w', encoding='utf-8', newline='')
+writer = csv.DictWriter(csv_file, 
+                        fieldnames = ['username', 'fullName', 'title', 'location', 'website',\
+                            'bio', 'creatorType', 'patreonURL', 'joined', 'lastPosted', 'lastSeen',\
+                            'views', 'invitedBy', 'trustLevel', 'daysVisited', 'readTime', 'recentReadTime',\
+                            'topicsViewed', 'postsRead', 'likesGiven', 'topicsCreated', 'postsCreated',\
+                            'likesReceived'])
+writer.writeheader()
 
+for i in range(len(users[:3])):
     # open up a driver for each user page
     chromeOptions = webdriver.ChromeOptions()
     prefs = {'profile.managed_default_content_settings.images':2}
     chromeOptions.add_experimental_option('prefs', prefs)
     driver = webdriver.Chrome(options=chromeOptions)
+    
+    driver.get(users[i])
+    
+    try:
+        username = driver.find_element_by_xpath('.//*/h1[@class="username"]').text
+    except:
+        username = np.nan
+    try:
+        fullName = driver.find_element_by_xpath('.//*/h2[@class="full-name"]').text
+    except:
+        fullName = np.nan
 
-    username = 
+    userDict = {}
+
+    userDict['username'] = username
+    userDict['fullName'] = fullName
+
+    writer.writerow(userDict)
+
+    driver.quit()
 
 
 # # scroll down to the very bottom of the page
